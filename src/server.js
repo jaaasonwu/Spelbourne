a1219670102340bbfe2404f10ee3324b75a2117c
Raymond <raymond13513@gmail.com>
2017-03-12 17:05:01 +1100

    Use google auth for login

--- src/server.js
+++ src/server.js
@@ -4,9 +4,14 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const path = require('path');
 const logger = require('morgan');
+const GoogleAuth = require('google-auth-library');
+const CLIENT_ID = '523892883118-akcfr1q4mg78ffrfttcil34emeuqkpd5.apps.googleusercontent.com'
 
 // Defaul port to 8888 or use env PORT
 const PORT = process.env.PORT || 8888;
+
+const auth = new GoogleAuth;
+const client = new auth.OAuth2(CLIENT_ID, '', '')
 
 // Create express app
 var app = express();
@@ -21,4 +26,23 @@ app.use(express.static(path.join(__dirname,'view')));
 // Start the server
 app.listen(PORT, function() {
     console.log("server started at port:", PORT);
+});
+
+app.post('/tokensignin', function(req, res) {
+    let token = req.body.idtoken;
+    client.verifyIdToken(
+        token,
+        CLIENT_ID,
+        function(err, login) {
+            if (err) {
+                console.log(err);
+                res.end();
+                return;
+            }
+
+            let payload = login.getPayload();
+            let userid = payload['sub'];
+            res.send(userid);
+        }
+    );
 });
