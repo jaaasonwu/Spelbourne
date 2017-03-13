230088742191c68a5c3eabc3c24be096e18d59b3
Patrick Zhai <zhai7631@gmail.com>
2017-03-13 15:30:51 +1100

    Add module mongoose

--- src/server.js
+++ src/server.js
@@ -1,39 +1,41 @@
-// Requre usefule modules
+// Require useful modules
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const path = require('path');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 const GoogleAuth = require('google-auth-library');
-const CLIENT_ID = '523892883118-akcfr1q4mg78ffrfttcil34emeuqkpd5.apps.googleusercontent.com'
+const CLIENT_ID = '523892883118-akcfr1q4mg78ffrfttcil34emeuqkpd5.apps.googleusercontent.com';
 
-// Defaul port to 8888 or use env PORT
+// Default port to 8888 or use env PORT
 const PORT = process.env.PORT || 8888;
 
 const auth = new GoogleAuth;
 const client = new auth.OAuth2(CLIENT_ID, '', '')
 
 // Create express app
-var app = express();
+let app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Use view as static folder
-app.use(express.static(path.join(__dirname,'views')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("server started at port:", PORT);
 });
 
-app.post('/tokensignin', function(req, res) {
+// The api to authorize the token and get the information
+app.post('/tokensignin', function (req, res) {
     let token = req.body.idtoken;
     client.verifyIdToken(
         token,
         CLIENT_ID,
-        function(err, login) {
+        function (err, login) {
             if (err) {
                 console.log(err);
                 res.end();
