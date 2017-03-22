d4a7b8d9e0adc8bbefdb4a0ccdac08ad8a8bdcb1
Patrick Zhai <zhai7631@gmail.com>
2017-03-22 13:43:47 +1100

    Now logins are available

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -1,8 +1,12 @@
 // load dependencies
-const LocalStrategy = require('passport-local');
+const LocalStrategy    = require('passport-local').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const GoogleStrategy   = require('passport-google-oauth').OAuth2Strategy;
 
 // load up the user model
 const User = require('./models/user');
+
+const configAuth = require('./auth');
 
 // exposed function
 module.exports = function (passport) {
@@ -85,6 +89,94 @@ module.exports = function (passport) {
                 }
 
                 return done(null, user);
+            });
+        }));
+
+    // Facebook Login
+    passport.use(new FacebookStrategy({
+            clientID: configAuth.facebookAuth.clientID,
+            clientSecret: configAuth.facebookAuth.clientSecret,
+            callbackURL: configAuth.facebookAuth.callbackURL,
+            profileFields: ["emails", "displayName"]
+        },
+        function (token, refreshToken, profile, done) {
+            // asynchronous
+            process.nextTick(function () {
+                User.findOne({'facebook.id': profile.id}, function (err, user) {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    // if the user is found, then log them in
+                    if (user) {
+                        return done(null, user);
+                    } else {
+                        // new user
+                        let newUser = new User();
+
+                        newUser.facebook.id    = profile.id;
+                        newUser.facebook.token = token;
+                        newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                        console.log(profile);
+                        newUser.facebook.email = profile.emails[0].value;
+
+                        // save to the database
+                        newUser.save(function (err) {
+                            if (err) {
+                                throw err;
+                            }
+
+                            return done(null, newUser);
+                        });
+                    }
+                });
+            });
+        }));
+
+    // google login
+    passport.use(new GoogleStrategy({
+
+            clientID: configAuth.googleAuth.clientID,
+            clientSecret: configAuth.googleAuth.clientSecret,
+            callbackURL: configAuth.googleAuth.callbackURL,
+            returnURL: configAuth.googleAuth.callbackURL,
+            realm: "http://localhost:8888/"
+
+        },
+        function (token, refreshToken, profile, done) {
+
+            // make the code asynchronous
+            // User.findOne won't fire until we have all our data back from Google
+            process.nextTick(function () {
+
+                // try to find the user based on their google id
+                User.findOne({'google.id': profile.id}, function (err, user) {
+                    if (err)
+                        return done(err);
+
+                    if (user) {
+
+                        // if a user is found, log them in
+                        return done(null, user);
+                    }
+
+                    // If the user isnt in our database, create a new user
+                    let newUser = new User();
+
+                    // Set all of the relevant information
+                    newUser.google.id    = profile.id;
+                    newUser.google.token = token;
+                    newUser.google.name  = profile.displayName;
+                    newUser.google.email = profile.emails[0].value; // pull the first email
+
+                    // save the user
+                    newUser.save(function (err) {
+                        if (err)
+                            throw err;
+                        return done(null, newUser);
+                    });
+
+                });
             });
         }));
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -44,6 +44,27 @@ module.exports = function (app, passport) {
         });
     });
 
+    // Facebook routes
+    // route for facebook authentication and login
+    app.get('/auth/facebook',passport.authenticate('facebook',{ scope: 'email'}));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+    passport.authenticate('facebook',{
+        successRedirect : '/profile',
+        failureRedirect : '/'
+    }));
+
+    // Google routes
+    app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+    // the callback after google has authenticated the user
+    app.get('/auth/google/callback',
+        passport.authenticate('google', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
     // Logout page
     app.get('/logout', function (req, res) {
         req.logout();
