1dadfe84146953a131aad8b2fc7fe4139de35fce
studentKkalya <kkalya@student.unimelb.edu.au>
2017-04-05 20:02:32 +1000

    Add create event in the front end

--- views/page/createEvent/createEventController.js
+++ views/page/createEvent/createEventController.js
@@ -1,5 +1,6 @@
 define(['app'], function (app) {
-    app.controller('createEventController', ['$scope', '$http', function($scope, $http) {
+    app.controller('createEventController', ['$scope', '$http', '$location',
+                    function($scope, $http , $location) {
         // These are mock data, will qurey from the server in the future
         $scope.sportsCategory = [
             "Tennis",
@@ -7,15 +8,36 @@ define(['app'], function (app) {
             "Soccer",
             "Basketball"
         ];
-
         $scope.startTime = ["9:00", "9:30", "10:00"];
 
         $scope.duration = ["30 min", "60 min", "90 min", "120 min"];
+
+        $scope.data = {
+            location: "",
+            description: "",
+            eventDate: "",
+            endDate: "",
+            visibility: "Friends",
+            selectedSport: $scope.sportsCategory[0]
+        };
+
 
         // Variables to bind to the front end
         $scope.selectedSport = ""
         $scope.selectedStartTime = ""
         $scope.selectedDuration = ""
-
+        $scope.createEvent = function () {
+            $http.post('/createEvent', $scope.data)
+            .then(
+                // success callback
+                function (res) {
+                    $location.path('/');
+                },
+                // failure callback
+                function (res) {
+                    console.log(res);
+                }
+            )
+        };
     }]);
 });
