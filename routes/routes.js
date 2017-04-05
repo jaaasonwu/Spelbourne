b1c9d78da8ed3d788e5e36426a621954abab028e
Raymond <raymond13513@gmail.com>
2017-04-05 18:58:45 +1000

    Add event data model

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,18 @@
-module.exports = function (app, passport) {
+module.exports = function (app, passport, eventService) {
     // Home page
     app.get('/',function(req, res) {
         res.render('index.ejs');
     });
+
     app.get('/:page', function (req, res) {
         res.render('index.ejs');
+    });
+
+    app.post('/createEvent', function(req, res) {
+        let event = req.body;
+        eventService.createEvent(event.location, event.organizerID, event.participants, event.description,
+                event.createEventDate, event.eventDate, event.endDate, event.visibility);
+        res.end();
     });
 
     // Process the login form
