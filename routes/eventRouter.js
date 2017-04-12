d14859e4a975e3ccf25191878a2098bcec0f9b86
Raymond <raymond13513@gmail.com>
2017-04-12 18:25:09 +1000

    Add organizerID to the create eventAPI

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -3,10 +3,13 @@ const route = express.Router();
 const eventService = require('../lib/events/eventService.js');
 
 route.post('/createEvent', function(req, res) {
-    let event = req.body;
-    eventService.createEvent(event.location, event.organizerID, event.participants, event.description,
-        event.createEventDate, event.eventDate, event.endDate, event.visibility);
-
+    if (req.isAuthenticated()) {
+        let event = req.body;
+        organizerID = req.user._id;
+        eventService.createEvent(event.location, organizerID, event.participants, event.description,
+                event.createEventDate, event.eventDate, event.endDate, event.visibility);
+        res.end();
+    };
     res.end();
 });
 
--- routes/routes.js
+++ routes/routes.js
@@ -21,6 +21,7 @@ function isLoggedIn(req, res, next) {
     }
 
     // if they aren't redirect them to the home page
-    res.redirect('/');
+    let errMsg = {"Error": true};
+    res.json(errMsg);
 
 }
