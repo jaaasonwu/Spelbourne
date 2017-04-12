401e4c835637b19d115661aff515fd993d20c955
Jason Wu <jaaasonwu@gmail.com>
2017-04-12 18:47:25 +1000

    Add description box

--- views/page/createEvent/createEventController.js
+++ views/page/createEvent/createEventController.js
@@ -39,5 +39,13 @@ define(['app'], function (app) {
                 }
             )
         };
+
+        var mapOptions = {
+            zoom: 4,
+            center: new google.maps.LatLng(40.0000, -98.0000),
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        }
+
+        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
     }]);
 });
