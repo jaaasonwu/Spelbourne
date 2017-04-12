9596d1291eaea0e766bd6082f7f2d3aaf2579fc1
Raymond <raymond13513@gmail.com>
2017-04-12 17:57:28 +1000

    Create get event API

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -10,13 +10,15 @@ route.post('/createEvent', function(req, res) {
     res.end();
 });
 
-route.post('/getEvent', function(req, res) {
-    let event = req.body;
-
-    eventService.createEvent(event.location, event.organizerID, event.participants, event.description,
-        event.createEventDate, event.eventDate, event.endDate, event.visibility);
-
-    res.end();
+route.get('/getEvent/:eventID', function(req, res) {
+    let eventID = req.params.eventID;
+    eventService.getEvent(eventID, function (err, data) {
+        if (err) {
+            res.status(500).send("No such event");
+            return;
+        }
+        res.send(data);
+    });
 });
 
 module.exports = route;
