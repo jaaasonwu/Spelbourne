535f19c88a5ed45c497f3f52f0fd659f7268e65f
Jason Wu <jaaasonwu@gmail.com>
2017-04-13 13:58:14 +1000

    Allow searching for places and show it on google map on create event page. Auto complete also implemented.

--- views/page/createEvent/createEventController.js
+++ views/page/createEvent/createEventController.js
@@ -41,11 +41,57 @@ define(['app'], function (app) {
         };
 
         var mapOptions = {
-            zoom: 4,
-            center: new google.maps.LatLng(40.0000, -98.0000),
-            mapTypeId: google.maps.MapTypeId.TERRAIN
+            zoom: 14,
+            center: new google.maps.LatLng(-37.7964, 144.9612),
+            mapTypeId: 'roadmap'
         }
 
         $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        $scope.search = new google.maps.places.SearchBox(document.getElementById('locationInput'));
+        $scope.search.addListener('places_changed', function() {
+            var places = $scope.search.getPlaces();
+
+            if (places.length == 0) {
+                return;
+            }
+            var markers = [];
+
+            // Clear out the old markers.
+            markers.forEach(function(marker) {
+                marker.setMap(null);
+            });
+
+            // For each place, get the icon, name and location.
+            var bounds = new google.maps.LatLngBounds();
+            places.forEach(function(place) {
+                if (!place.geometry) {
+                    console.log("Returned place contains no geometry");
+                    return;
+                }
+                var icon = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25)
+                };
+
+                // Create a marker for each place.
+                markers.push(new google.maps.Marker({
+                    map: $scope.map,
+                    icon: icon,
+                    title: place.name,
+                    position: place.geometry.location
+                }));
+
+                if (place.geometry.viewport) {
+                    // Only geocodes have viewport.
+                    bounds.union(place.geometry.viewport);
+                } else {
+                    bounds.extend(place.geometry.location);
+                }
+            });
+            $scope.map.fitBounds(bounds);
+        });
     }]);
 });
