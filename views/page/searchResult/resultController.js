14c1a2d4faf5794ecec04389d3068f4081360907
Jason Wu <jaaasonwu@gmail.com>
2017-04-26 17:49:25 +1000

    Use icon instead of image in the search result page

--- views/page/searchResult/resultController.js
+++ views/page/searchResult/resultController.js
@@ -26,21 +26,21 @@ define(['app'], function (app) {
             // create a message to display in our view
             $scope.eventList = [
                 {
-                    img: "/images/tennis.jpg",
+                    img: "/images/sportsicon/tennisball.svg",
                     type: "Tennis",
                     place: "ERC",
                     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis porttitor lacus, non pulvinar mi. Morbi vestibulum augue in venenatis consectetur. Praesent auctor in libero gravida ultrices. Donec sagittis dapibus iaculis. Duis lectus justo, efficitur et est quis, auctor accumsan sem. Donec gravida risus eget posuere bibendum. Vivamus iaculis tortor diam. Curabitur vitae pharetra dui, sit amet vehicula nulla. Ut dolor dolor, congue quis tincidunt vulputate, luctus sed libero. Morbi viverra purus id auctor dapibus. Duis sem arcu, ultrices sit amet dui ac, eleifend volutpat est. Mauris interdum dolor vel suscipit facilisis. Praesent elementum nunc sed sem iaculis dapibus ut eget dui.",
                     skLevel: "master"
                 },
                 {
-                    img: "/images/football.jpg",
+                    img: "/images/sportsicon/football.svg",
                     type: "Soccer",
                     place: "ERC",
                     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis porttitor lacus, non pulvinar mi. Morbi vestibulum augue in venenatis consectetur. Praesent auctor in libero gravida ultrices. Donec sagittis dapibus iaculis. Duis lectus justo, efficitur et est quis, auctor accumsan sem. Donec gravida risus eget posuere bibendum. Vivamus iaculis tortor diam. Curabitur vitae pharetra dui, sit amet vehicula nulla. Ut dolor dolor, congue quis tincidunt vulputate, luctus sed libero. Morbi viverra purus id auctor dapibus. Duis sem arcu, ultrices sit amet dui ac, eleifend volutpat est. Mauris interdum dolor vel suscipit facilisis. Praesent elementum nunc sed sem iaculis dapibus ut eget dui.",
                     skLevel: "master"
                 },
                 {
-                    img: "/images/tennis.jpg",
+                    img: "/images/sportsicon/tennisball.svg",
                     type: "Tennis",
                     place: "ERC",
                     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis porttitor lacus, non pulvinar mi. Morbi vestibulum augue in venenatis consectetur. Praesent auctor in libero gravida ultrices. Donec sagittis dapibus iaculis. Duis lectus justo, efficitur et est quis, auctor accumsan sem. Donec gravida risus eget posuere bibendum. Vivamus iaculis tortor diam. Curabitur vitae pharetra dui, sit amet vehicula nulla. Ut dolor dolor, congue quis tincidunt vulputate, luctus sed libero. Morbi viverra purus id auctor dapibus. Duis sem arcu, ultrices sit amet dui ac, eleifend volutpat est. Mauris interdum dolor vel suscipit facilisis. Praesent elementum nunc sed sem iaculis dapibus ut eget dui.",
