0c777bc084a13c09b6ef3e7d2cc450178e771632
Raymond <raymond13513@gmail.com>
2017-05-03 19:45:37 +1000

    Remove unused code

--- views/services/userService.js
+++ views/services/userService.js
@@ -1,7 +1,7 @@
 define(['angularAMD'], function () {
     var app = angular.module('userService', []);
     app.service('userService', ['$http', '$location', '$window',
-        function ($http, $location, $window) {
+                function ($http, $location, $window) {
         var getUserProfile = function (userID, success, failure) {
             $http.get('/user/getUserProfile/' + userID).then(success, failure);
         }
