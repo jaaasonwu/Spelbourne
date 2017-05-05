d7437f1d15766d175dc28d1761d3946b272db416
studentKkalya <kkalya@student.unimelb.edu.au>
2017-05-06 03:16:13 +1000

    dashboard styling and navigationbar styling

--- views/page/profile/profileController.js
+++ views/page/profile/profileController.js
@@ -2,17 +2,30 @@ define(['app'], function (app) {
     app.controller('profileController',
         ['$scope', '$http', '$location', '$rootScope','$routeParams','userService','eventService', 'adminService',
         function ($scope, $http, $location,$rootScope ,$routeParams, userService, eventService, adminService) {
-            console.log($scope.userID);
+            //prevent access by unauthorized
+            if(!$rootScope.username){
+                $location.path('/');
+            }
+            //fetch profile info
+
+
             userService.getUserProfile(
                 $rootScope.userID,
                 function(res) {
-                    console.log(res.data);
                     $scope.events = [];
                     res.data.events.forEach(function (eventID) {
                         eventService.getEvent(
                             eventID,
                             function (res) {
+                                eventService.getIcon(
+                                  res.data.sportType,
+                                    function(path){
+                                        res.data.img = path.data;
+                                    }
+                                );
+                                res.data.formatDate = new Date(res.data.createEventDate).toDateString();
                                 $scope.events.push(res.data);
+
                             }
                         );
                     });
@@ -21,6 +34,11 @@ define(['app'], function (app) {
                     console.log(res.data.msg[0]);
                 }
             );
+            $scope.eventsMessage = "";
+            //console.log($scope.events.toString());
+            $scope.home = function(){
+                $location.path('/');
+            }
 
             $scope.viewEvent = function (event) {
                 console.log(event._id);
