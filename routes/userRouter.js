cc921f5bce1446bcf83c4e166e400921536ed863
Raymond <raymond13513@gmail.com>
2017-05-14 23:42:45 +1000

    Remove update profile

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -13,24 +13,5 @@ route.get('/getUserProfile/:userID', function(req, res) {
         res.send(data);
     });
 });
-route.post('/updateProfile', function(req, res) {
-    if (req.isAuthenticated()) {
-        let e = req.body;
-        userID = req.user._id;
-        data = req.body;
-        userProfileService.updateUserProfile(userID,data.name,data.phoneNumber,data.img, function(err){
-            if(err){
-                res.status(500).send("Update error");
-            }
-            else {
-                res.end();
-            }
-
-        });
-    } else {
-        // 401 means unauthorized
-        res.status(401).json({success: false, msg: '401'});
-    }
-});
 
 module.exports = route;
