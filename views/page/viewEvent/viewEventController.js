7510540373d399fdfe95b973e2c0751a47018cd7
Jason Wu <jaaasonwu@gmail.com>
2017-05-14 14:47:44 +1000

    Add facebook share. Line up participants on the view event page.

--- views/page/viewEvent/viewEventController.js
+++ views/page/viewEvent/viewEventController.js
@@ -19,6 +19,34 @@ define(['app'], function (app) {
             );
         };
 
+        initFB = function() {
+            window.fbAsyncInit = function() {
+                FB.init({
+                    appId      : '1357124691000611',
+                    xfbml      : true,
+                    version    : 'v2.9'
+                });
+                FB.AppEvents.logPageView();
+            };
+
+            (function(d, s, id){
+                let js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        }
+        initFB();
+
+        $scope.fbShare = function () {
+            FB.ui({
+                method: 'share',
+                display: 'popup',
+                href: $window.location.href,
+            }, function(response){});
+        }
+
         eventService.getEvent(
             eventID,
             function (res) {
