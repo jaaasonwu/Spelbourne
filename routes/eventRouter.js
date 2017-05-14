707c6c71cce05f69916cec2b37b5c58e8efab5b2
zhaih <zhaih@users.noreply.github.com>
2017-05-14 17:37:16 +1000

    Update eventRouter.js

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -45,7 +45,6 @@ route.post('/joinEvent', function(req, res) {
         let eventID = e.eventID;
         console.log(eventID);
         userID = req.user._id;
-        console.log(req);
         eventService.joinEvent(eventID, userID, function (err) {
             if (err) {
                 res.status(500).send(err);
