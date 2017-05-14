6af192f5e20bf49c9ac7b3b8708df93f8efdc815
zhaih <zhaih@users.noreply.github.com>
2017-05-14 17:37:57 +1000

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,6 @@ const userProfileService = require('../lib/user/userProfileService.js');
 
 route.get('/getUserProfile/:userID', function(req, res) {
     let userID = req.params.userID;
-    console.log(req.toString());
     userProfileService.getUser(userID, function (err, data) {
         if (err) {
             res.status(500).send("No such user");
