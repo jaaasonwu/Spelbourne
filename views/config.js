648759fbbe25970b4f2346aaedb228c182a9f517
Patrick Zhai <zhai7631@gmail.com>
2017-05-14 15:41:27 +1000

    Change all front end dependency to cdn

--- views/config.js
+++ views/config.js
@@ -3,17 +3,17 @@
 requirejs.config({
     baseUrl: "",
     paths: {
-        'angular': "/dependency/angular/angular.min",
-        'angular-route': "/dependency/angular/angular-route",
-        'angular-resource': "/dependency/angular/angular-resource.min",
-        'angular-filter': "/dependency/angular/angular-filter",
-        'jQuery': "/dependency/jquery/jquery.min",
-        'bootstrap': "/dependency/bootstrap/js/bootstrap",
-        'angularAMD':"/dependency/angular/angularAMD.min",
-        'ngMaterial': "/dependency/angular/angular-material.min",
-        'angular-animate': "/dependency/angular/angular-animate.min",
-        'angular-aria': "/dependency/angular/angular-aria.min",
-        'angular-ui' : "/dependency/angular/ui-bootstrap-tpls-1.3.3",
+        'angular': "//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min",
+        'angular-route': "//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-route",
+        'angular-resource': "//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-resource.min",
+        'angular-filter': "//cdnjs.cloudflare.com/ajax/libs/angular-filter/0.5.15/angular-filter",
+        'jQuery': "//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min",
+        'bootstrap': "//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min",
+        'angularAMD':"//cdn.jsdelivr.net/angular.amd/0.2/angularAMD.min",
+        'ngMaterial': "//ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min",
+        'angular-animate': "//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-animate.min",
+        'angular-aria': "//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-aria.min",
+        'angular-ui' : "//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/1.3.3/ui-bootstrap-tpls",
         'fb-sdk': '//connect.facebook.net/en_US/sdk'
     },
     shim: {
