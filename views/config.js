4a71596568b7f573e2e638927a63426259a084c7
Patrick Zhai <zhai7631@gmail.com>
2017-05-18 01:07:52 +1000

    Update jquery

--- views/config.js
+++ views/config.js
@@ -7,7 +7,7 @@ requirejs.config({
         'angular-route': "//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-route",
         'angular-resource': "//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-resource.min",
         'angular-filter': "//cdnjs.cloudflare.com/ajax/libs/angular-filter/0.5.15/angular-filter",
-        'jQuery': "//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min",
+        'jQuery': "//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min",
         'bootstrap': "//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min",
         'angularAMD':"//cdn.jsdelivr.net/angular.amd/0.2/angularAMD.min",
         'ngMaterial': "//ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min",
