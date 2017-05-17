71b32ff78cef8e4e239a22b36bd62706e3441d10
Patrick Zhai <zhai7631@gmail.com>
2017-05-17 19:24:42 +1000

    Make the recommendations in welcome controller, wait for profile preference

--- views/page/welcome/welcomeController.js
+++ views/page/welcome/welcomeController.js
@@ -1,17 +1,14 @@
 define(['app', 'angular-filter'], function (app) {
     // create the controller and inject Angular's $scope
-    app.controller('welcomeController', ['$scope', '$http', '$location', 'eventService',
-            function($scope, $http, $location, eventService) {
-        /*
-         * Get the recommendations
-         * For now, we only the the first 6 events in the databases
-         * Will fix this in the future
-         */
+    app.controller('welcomeController', ['$scope','$rootScope', '$http', '$location', 'eventService', 'userService',
+            function($scope, $rootScope, $http, $location, eventService, userService) {
+        var events;
         eventService.getEventList(
             // success callback
             function (res) {
-                $scope.events = res.data.slice(0, 6);
-                $scope.events.forEach(function(event) {
+                events = res.data;
+                // $scope.events = res.data.slice(0, 6);
+                events.forEach(function(event) {
                     startDate = new Date(event.startDate);
 
                     event.startDate = startDate.toLocaleDateString();
@@ -23,6 +20,48 @@ define(['app', 'angular-filter'], function (app) {
                         }
                     );
                 });
+                /*
+                 * Get the recommendations
+                 * For user not logged in, we only the the first 6 events in the databases
+                 * For those who logged in, the preferred sports will be in first order
+                 */
+                if ($rootScope.userID) {
+                    userService.getUserProfile($rootScope.userID,
+                        // success callback
+                        function(res) {
+                            var pref = res.data;
+                            // if the user has some preference
+                            if (pref.interests && pref.interests !== []) {
+                                var interested = [];
+                                var notInterested = [];
+                                // classify the events
+                                events.forEach(function(e) {
+                                   if (pref.interests.indexOf(e.sportType) > 0) {
+                                       interested.push(e);
+                                   } else {
+                                       notInterested.push(e);
+                                   }
+                                });
+                                // make the event list
+                                if (interested.length < 6){
+                                    $scope.events = interested.concat(notInterested.slice(start,6 - interested.length));
+                                } else {
+                                    $scope.events =interested.slice(0,6);
+                                }
+                            } else {
+                                // if no preference
+                                $scope.events = events.slice(0,6);
+                            }
+                        },
+                        // failure callback
+                        function(res) {
+                            console.log(res);
+                            $scope.events = events.slice(0,6);
+                        }
+                    )
+                } else {
+                    $scope.events = events.slice(0,6);
+                }
             },
             // failure callback
             function (res) {
